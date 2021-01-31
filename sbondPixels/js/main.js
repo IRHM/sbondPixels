33bcd415bbfcd1aa2d4a5d720aa17832c430efea
IRHM <37304121+IRHM@users.noreply.github.com>
2021-01-31 10:46:04 +0000

    Normalise pxlBox
    so that it doesn't change site-to-site depending on if they change tag styles globally.
    
    Also format js.

--- sbondPixels/js/main.js
+++ sbondPixels/js/main.js
@@ -1,4 +1,4 @@
-function onError(e){
+function onError(e) {
   console.error(e);
 }
 
@@ -6,70 +6,71 @@ function onError(e){
 var pxlBox = document.createElement("SPAN");
 
 // Style element
-var pxlBoxS = pxlBox.style;
+pxlBox.style.cssText = `
+  display: none !important;
+  position: fixed !important;
+  z-index: 9999999999 !important;
+  top: 5px !important;
+  left: 5px !important;
+  background-color: black;
+  color: white !important;
+  border: 1px solid white;
+  font-family: sans-serif !important;
+  font-size: 16px !important;
+  letter-spacing: normal !important;
+  line-height: 1 !important;
+  padding: 4px !important;
+`;
 
-pxlBoxS.display = "none";
-pxlBoxS.position = "fixed";
-pxlBoxS.zIndex = "9999999999";
-pxlBoxS.top = "5px";
-pxlBoxS.left = "5px";
-pxlBoxS.backgroundColor = "black";
-pxlBoxS.color = "white";
-pxlBoxS.padding = "2px 5px";
-
-function showPxBx(show){
-	if(show){
-		pxlBoxS.display = "";
-		pxlBox.textContent = getRes();
-		enabled = 1;
-	}
-	else{
-		pxlBoxS.display = "none";
-		enabled = 0;
-	}
+function showPxBx(show) {
+  if (show) {
+    pxlBox.style.display = "";
+    pxlBox.textContent = getRes();
+    enabled = 1;
+  } else {
+    pxlBox.style.display = "none";
+    enabled = 0;
+  }
 }
 
 // Get window dimensions
-function getRes(){
+function getRes() {
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight;
-  
-  return windowWidth + ' x ' + windowHeight;
+
+  return windowWidth + " x " + windowHeight;
 }
 
 // Listen for when to enable/disable px bx
-browser.storage.onChanged.addListener(function(data){
-	if(data.settings.newValue.enabled){
-		// Enable
-		showPxBx(1);
-	}
-	else{
-		// Disable
-		showPxBx(0);
-	}
+browser.storage.onChanged.addListener(function (data) {
+  if (data.settings.newValue.enabled) {
+    // Enable
+    showPxBx(1);
+  } else {
+    // Disable
+    showPxBx(0);
+  }
 });
 
 // Re-set res on resize
-window.addEventListener("resize", function(event){
-	if(enabled){
-		showPxBx(1);
-	}
-	else{
-		showPxBx(0);
-	}
+window.addEventListener("resize", function (event) {
+  if (enabled) {
+    showPxBx(1);
+  } else {
+    showPxBx(0);
+  }
 });
 
 // Set default display for px bx
-browser.storage.local.get().then(function(data){
+browser.storage.local.get().then(function (data) {
   enabled = data.settings.enabled;
-	if(enabled){
-		// Enable
-		showPxBx(1);
-	}
-	else{
-		// Disable
-		showPxBx(0);
-	}
+  if (enabled) {
+    // Enable
+    showPxBx(1);
+  } else {
+    // Disable
+    showPxBx(0);
+  }
 });
 
 // Append element to end of body
--- sbondPixels/js/state.js
+++ sbondPixels/js/state.js
@@ -1,8 +1,8 @@
-function onError(e){
+function onError(e) {
   console.error(e);
 }
 
-async function storeState(state){
+async function storeState(state) {
   browser.storage.local.set({
     settings: {
       enabled: state
@@ -10,27 +10,25 @@ async function storeState(state){
   });
 }
 
-async function checkState(restoredSettings){
-	var settings = {
-		enabled: 1
-	}
+async function checkState(restoredSettings) {
+  var settings = {
+    enabled: 1
+  };
 
-	if(!restoredSettings.settings){
-    browser.storage.local.set({settings});
+  if (!restoredSettings.settings) {
+    browser.storage.local.set({ settings });
+  } else {
+    var enabled = restoredSettings.settings.enabled;
+
+    if (enabled) {
+      await storeState(0);
+    } else {
+      await storeState(1);
+    }
   }
-	else{
-		var enabled = restoredSettings.settings.enabled;
-	
-		if(enabled){
-			await storeState(0);
-		}
-		else{
-			await storeState(1);
-		}
-	}
 }
 
 browser.browserAction.onClicked.addListener((tab) => {
-	const getStorage = browser.storage.local.get();
-	getStorage.then(checkState, onError);
+  const getStorage = browser.storage.local.get();
+  getStorage.then(checkState, onError);
 });
